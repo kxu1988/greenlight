9069c8b5e5c9e257dc9dca20aab6d8ebc71a449b
kxu <kxu@ec2star.com>
2020-09-01 09:31:27 +0800

    update

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -1,17 +1,50 @@
 class ScreensController < ApplicationController
   def index
+    @screen_1= Screen.find_by(number:1)
+    @screen_2= Screen.find_by(number:2)
+    @screen_3= Screen.find_by(number:3)
+    @screen_4= Screen.find_by(number:4)
+    @screen_5= Screen.find_by(number:5)
+    @screen_6= Screen.find_by(number:6)
+    @screen_7= Screen.find_by(number:7)
+    @screen_8= Screen.find_by(number:8)
+    @screen_9= Screen.find_by(number:9)
   end
 
-  def edit
+  def update
+    screen_id = params[:id]
+    channels = JSON.parse(params[:channels])
+
+    screen = Screen.find_or_create_by(number:screen_id)
+    screen.name = params[:device_name]
+    screen.save
+
+    Channel.where(number:screen_id).destroy_all
+
+    channels.each do |channel|
+      id = channel["id"]
+      device = channel["device"]
+      channel = Channel.new
+      channel.number = screen_id
+      channel.channel = id
+      channel.name = device
+      channel.save
+    end
+
+    @result = 1
+
+    respond_to do |format|
+      format.json { render :xml => @result.to_json }
+    end
   end
   
   def add
   end
 
   def channels
-    id = params[:id]
+    @device_id = params[:id]
     @device = params[:name]
-    channels_url = "http://119.253.35.41:10000/api/v1/device/channellist?serial=#{id}"
+    channels_url = "http://119.253.35.41:10000/api/v1/device/channellist?serial=#{@device_id}"
     result = JSON.parse(RestClient.get(channels_url))
     @channels = result['ChannelList']
 
@@ -22,7 +55,7 @@ class ScreensController < ApplicationController
   end
 
   def all_device
-    id = params[:id]
+    @id = params[:id]
     devices_url = "http://119.253.35.41:10000/api/v1/device/list"
     result = JSON.parse(RestClient.get(devices_url))
     @devices = result['DeviceList']
